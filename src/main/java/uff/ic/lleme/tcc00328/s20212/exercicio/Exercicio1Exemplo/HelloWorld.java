d45d9bedff1457bb82d67ac771370bb3fd2a722b
Luiz Torre <luiztorre@id.uff.br>
2021-11-08 11:30:43 -0300

    Atividade 1 - Exemplo - Hello World

---
+++ src/main/java/uff/ic/lleme/tcc00328/s20212/exercicio/Exercicio1Exemplo/HelloWorld.java
@@ -0,0 +1,13 @@
+package uff.ic.lleme.tcc00328.s20212.exercicio.Exercicio1Exemplo;
+
+/**
+ *
+ * @author luizm
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+         System.out.println("Hello World( Alô Mundo)");
+    }
+    
+}
--- src/main/java/uff/ic/lleme/tcc00328/s20212/prova/p1/LuizAndre.java
+++ src/main/java/uff/ic/lleme/tcc00328/s20212/prova/p1/LuizAndre.java
@@ -3,6 +3,6 @@ package uff.ic.lleme.tcc00328.s20212.prova.p1;
 public class LuizAndre {
 
     public static void main(String[] args) {
-        System.out.println("Alô mundo!!!!!!!!!");
+        System.out.println("Hello World!!!!!!!!!");
     }
 }
